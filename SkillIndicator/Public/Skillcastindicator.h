3905ccf773aeee6702ec102181ebd3674ea0e2e1
jokate <kkkk4017@naver.com>
2025-04-12 13:22:49 +0900

    프로젝트 개설

---
+++ SkillIndicator/Public/Skillcastindicator.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Skillcastindicator.generated.h"
+
+UCLASS()
+class SKILLINDICATOR_API ASkillcastindicator : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASkillcastindicator();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
---
+++ SkillIndicator/SkillIndicatorGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SkillIndicatorGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASkillIndicatorGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASkillIndicatorGameMode();
+};
+
+
+
